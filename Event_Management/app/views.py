1ed49e8526c90f6222eaee11da7bc9ad7973389d
devsmarak972 <smarakkanjilal@gmail.com>
2024-03-01 13:14:33 +0530

    events page

--- Event_Management/app/views.py
+++ Event_Management/app/views.py
@@ -4,3 +4,7 @@ app_views = Blueprint('app_views', __name__)
 @app_views.route('/')
 def index():
     return render_template('test.html', name='orld')
+@app_views.route('/event')
+def events():
+    events=[{"title":"Event 1","num_p":200,"desc":"this is the event description.this is the event descriptionthis is the event descriptionthis is the event descriptionthis is the event descriptionthis is the event descriptionthis is the event description","tags":["hello","tags1","tag2"]}]
+    return render_template('events.html', name='events',events=events)
